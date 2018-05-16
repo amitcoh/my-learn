4e30378037ab7bd3a188e7c4adb35fb3f39261ac
amitcoh <amitcoh@bynet.co.il>
2018-05-16 21:36:05 +0300

    Added Posts Service

---
+++ src/app/Post.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by amitcoh on 13/05/2018.
+ */
+export class Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MessagesComponent } from './messages/messages.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PostsComponent } from './posts/posts.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'posts', component: PostsComponent },
   { path: '',   redirectTo: '/login', pathMatch: 'full' }
 ];
 
@@ -15,13 +19,16 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
-    MessagesComponent
+    MessagesComponent,
+    NavigationComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
       appRoutes
-    )
+    ),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
